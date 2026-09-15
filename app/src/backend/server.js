4adf3bb8d01c2e102a849fbdc5bb6881230d7c6d
Sk1ddl3s <2693147@students.wits.ac.za>
2026-09-15 12:29:59 +0200

    fix: cors error to separate frontend and backend

--- app/src/backend/server.js
+++ app/src/backend/server.js
@@ -53,6 +53,7 @@ const allowed_origins = [
 	'http://127.0.0.1:5173',
 	'http://localhost:3000',
 	'http://127.0.0.1:3000',
+	'https://website.adamas2aurum.workers.dev/',
 ]
 if (process.env.FRONTEND_URL) allowed_origins.push(process.env.FRONTEND_URL)
 
@@ -87,7 +88,8 @@ const session_middleware = session({
 	saveUninitialized: false,
 	cookie: {
 		httpOnly: true,
-		secure: false,
+		secure: true,
+		sameSite: 'none',
 		maxAge: 1000 * 60 * 60 * 24, // 24 hours
 	},
 })
