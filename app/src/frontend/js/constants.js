388741789fe28c595208f7efbefd71b956415207
delete nullptr <162191486+de1etenu11ptr@users.noreply.github.com>
2026-09-18 00:40:10 +0200

    fix: localhost links

--- app/src/frontend/js/constants.js
+++ app/src/frontend/js/constants.js
@@ -1,2 +1,2 @@
-export const API_BASE = 'https://localhost:3000'
-export const API_BASE_WS = 'wss://localhost:3000'
+export const API_BASE = 'http://localhost:3000'
+export const API_BASE_WS = 'ws://localhost:3000'
